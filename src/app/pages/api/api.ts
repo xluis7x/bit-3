f7e879368995855a8909d69e2cda389b2240a7cf
Luis <xluis.58x@gmail.com>
2025-06-22 21:15:29 -0500

    june 22 finished

--- src/app/pages/api/api.ts
+++ src/app/pages/api/api.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Navigation } from '../../shared/navigation/navigation';
+import { PotterApi } from '../../services/api';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-api',
-  imports: [Navigation],
+  imports: [Navigation, NgOptimizedImage],
   templateUrl: './api.html',
   styleUrl: './api.css'
 })
-export class Api {
+export class Api implements OnInit{
+  private api = inject(PotterApi)
+  books!: any[]; 
+  chars!: any[];
+  houses!: any[];
+  spells!: any[];
+
+  ngOnInit(): void {
+    this.api.getBooks().subscribe((res:any)=> {
+      this.books = res;
+      console.log("books: ", this.books)
+    })
+    this.api.getChar().subscribe((res:any)=> {
+      this.chars = res;
+      console.log(this.chars)
+    })
+    this.api.getHouses().subscribe((res:any)=> {
+      this.houses = res;
+      console.log(this.houses)
+    })
+    this.api.getSpells().subscribe((res:any)=> {
+      this.spells = res;
+      console.log(this.spells)
+    })
+  }
 
 }
--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,12 +1,27 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Injectable({
   providedIn: 'root'
 })
-export class Api {
+export class PotterApi {
   constructor() {}
   private httpClient = inject(HttpClient)
+  private potterApiBooks = 'https://potterapi-fedeperin.vercel.app/en/books';
+  private potterApiChar = 'https://potterapi-fedeperin.vercel.app/en/characters';
+  private potterApiHouses = 'https://potterapi-fedeperin.vercel.app/en/houses';
+  private potterApiSpells = 'https://potterapi-fedeperin.vercel.app/en/spells/random';
 
+  getBooks() {
+    return this.httpClient.get(this.potterApiBooks);
+  }
+  getChar() {
+    return this.httpClient.get(this.potterApiChar);
+  }
+  getHouses() {
+    return this.httpClient.get(this.potterApiHouses);
+  }
+  getSpells() {
+    return this.httpClient.get(this.potterApiSpells);
+  }
 }
